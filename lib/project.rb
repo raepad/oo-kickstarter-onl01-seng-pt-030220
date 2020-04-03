eb67b6ccf9ff3c408551c4f03268ef94d1d549c8
Rachel Padworski <rachelpadworski@gmail.com>
2020-04-03 00:05:36 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -7,8 +7,8 @@ class Project
   end
   
   def add_backer(backer)
-    @backers << Backer
-    Backer.backed_projects << Backer
+    @backers << backer
+    Backer.backed_projects << backer
   end
   
 end
